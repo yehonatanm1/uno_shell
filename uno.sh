1427db2ab9eb4542fb36fc249e73309ddc8d234d
yehonatanm1 <yehonatanm09@gmail.com>
2025-09-16 12:02:42 +0300

    Update uno.sh

--- uno.sh
+++ uno.sh
@@ -1,5 +1,5 @@
 # ה-WebHook של Discord
-$webhook = "https://discord.com/api/webhooks/XXXXXXXXX/XXXXXXXX"
+$webhook = "https://discord.com/api/webhooks/1380222529209241680/jOwJW5pI3_hLmcpsOVjSLsS3rZyYGiRDS49oKPa0myTKTAnRJ_vwqNhaFggDyJOfhji-"
 
 # מביא IP חיצונית מהשירות ipify
 $publicIP = (Invoke-RestMethod -Uri "https://api.ipify.org?format=json").ip
